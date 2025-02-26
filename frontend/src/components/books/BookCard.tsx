b51c5ea6bf3875ec8a5d5a6a4d941ac0f48f9be6
ban <summer.jyyy@gmail.com>
2025-02-26 17:54:47 +0900

    design: 책 목록 수정

--- frontend/src/components/books/BookCard.tsx
+++ frontend/src/components/books/BookCard.tsx
@@ -9,22 +9,23 @@ interface Props{
 }
 export default function BookCard({book}: Props){
   return(
-    // TODO: 링크 수정
-    <Link to={`/${book.id}`} className="w-fit h-64 flex gap-10 justify-center group">
-      <div className="w-48 h-full bg-book group-hover:shadow-md p-3">
-        <p className="font-semibold text-3xl text-myblue">{book.title}</p>
-      </div>
-
-      <div className="h-full w-xl p-5 flex flex-col justify-between group-hover:shadow-md">
-        <p className="text-2xl">{book.title}</p>
-        <p className="text-xl">{book.author}/{book.publisher}/{String(book.pub_date)}</p>
-        <p className="text-xl">{book.price}원</p>
-        <p>{book.description}</p>
-        <p>판매량 {book.sales} 재고 {book.stock}</p>
-        <div className="flex gap-5 justify-end w-full">
-          <EditDeleteBtns book={book}/>
+    <div className="w-fit h-64 relative">
+      <Link to={`/${book.id}`} className="w-full h-full flex gap-10 justify-center group peer">
+        <div className="w-48 h-full bg-book group-hover:shadow-md p-3">
+          <p className="font-semibold text-3xl text-myblue">{book.title}</p>
         </div>
+
+        <div className="h-full w-xl p-5 flex flex-col justify-between group-hover:shadow-md">
+          <p className="text-2xl">{book.title}</p>
+          <p className="text-xl">{book.author}/{book.publisher}/{String(book.pub_date)}</p>
+          <p className="text-xl">{book.price}원</p>
+          <p>{book.description}</p>
+          <p>판매량 {book.sales} 재고 {book.stock}</p>
+        </div>
+      </Link>
+      <div className="flex gap-5 absolute bottom-5 right-5 invisible peer-hover:visible hover:visible ">
+        <EditDeleteBtns book={book}/>
       </div>
-    </Link>
+    </div>
   )
 }
