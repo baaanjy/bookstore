588c7dd03ac869313eb86445d170e6d376d3d4cd
ban <summer.jyyy@gmail.com>
2025-02-26 21:53:23 +0900

    feat: 천단위 구분 적용

--- frontend/src/api/book.ts
+++ frontend/src/api/book.ts
@@ -1,10 +1,24 @@
 import api from "@/api/index.ts"
+import Book from "@/types/book"
+
+const REGION = 'ko-KR'
 
 export async function getBooks(){
   try{
     const response = await api.get("/books")
     if(response.data.success){
-      return response.data.data
+      const books = response.data.data
+
+      const formattedBooks = books.map((book:Book)=>{
+        return {
+          ...book,
+          price: book.price.toLocaleString(REGION),
+          sales: book.sales.toLocaleString(REGION),
+          stock: book.stock.toLocaleString(REGION)
+        }
+      })
+
+      return formattedBooks
     } else{
       console.log(response.data.message)
       return []
