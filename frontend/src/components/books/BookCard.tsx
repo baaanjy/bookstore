99fbd6ee298e54c49107a35f957a888ee2e30990
ban <summer.jyyy@gmail.com>
2025-02-27 15:43:58 +0900

    design: 카드 목록 반응형 수정

--- frontend/src/components/books/BookCard.tsx
+++ frontend/src/components/books/BookCard.tsx
@@ -9,20 +9,20 @@ interface Props {
 }
 export default function BookCard({ book }: Props) {
   return (
-    <div className="relative h-64 w-fit">
+    <div className="relative h-64 w-full">
       <Link
         to={`/${book._id}`}
-        className="group peer flex h-full w-full justify-center gap-10">
-        <div className="bg-book h-full w-48 p-3 group-hover:shadow-md">
-          <p className="text-myblue text-3xl font-semibold">{book.title}</p>
+        className="group peer grid grid-cols-4 h-full w-full justify-center gap-10">
+        <div className="bg-book h-full p-3 group-hover:shadow-md lg:block col-span-1 hidden">
+          <p className="text-myblue lg:text-3xl lg:font-semibold">{book.title}</p>
         </div>
 
-        <div className="flex h-full w-xl flex-col justify-between p-5 group-hover:shadow-md">
-          <p className="text-2xl">{book.title}</p>
-          <p className="text-xl">
+        <div className="flex h-full w-full col-span-4 lg:col-span-3 flex-col justify-between p-5 group-hover:shadow-md">
+          <p className="text-xl md:text-2xl">{book.title}</p>
+          <p className="text-base md:text-xl">
             {book.author}/{book.publisher}/{String(book.pub_date)}
           </p>
-          <p className="text-xl">{book.price.toLocaleString('ko-KR')}원</p>
+          <p className="text-base md:text-xl">{book.price.toLocaleString('ko-KR')}원</p>
           <p>{book.description}</p>
           <p>
             판매량 {book.sales.toLocaleString('ko-KR')} 재고{' '}
@@ -30,7 +30,7 @@ export default function BookCard({ book }: Props) {
           </p>
         </div>
       </Link>
-      <div className="invisible absolute right-5 bottom-5 flex gap-5 peer-hover:visible hover:visible">
+      <div className="hidden sm:invisible absolute right-5 bottom-5 sm:flex gap-5 sm:peer-hover:visible sm:hover:visible">
         <EditDeleteBtns book={book} />
       </div>
     </div>
